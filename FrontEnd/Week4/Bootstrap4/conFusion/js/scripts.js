cfbe2b383648a1b96c57f4173f38adf18955f7f3
Jannia Fajer <janniapaola@gmail.com>
2017-08-27 16:36:39 -1000

    First Angular Commit

--- FrontEnd/Week4/Bootstrap4/conFusion/js/scripts.js
+++ FrontEnd/Week4/Bootstrap4/conFusion/js/scripts.js
@@ -1,12 +1,32 @@
-$("#carousel-button").click(function(){
-    if ($("#carousel-button").children("span").hasClass('fa-pause')) {
-        $("#mycarousel").carousel('pause');
-        $("#carousel-button").children("span").removeClass('fa-pause');
-        $("#carousel-button").children("span").addClass('fa-play');
-    }
-    else if ($("#carousel-button").children("span").hasClass('fa-play')){
-        $("#mycarousel").carousel('cycle');
-        $("#carousel-button").children("span").removeClass('fa-play');
-        $("#carousel-button").children("span").addClass('fa-pause');
-    }
+$(document).ready(function() {
+    $("#carousel-button").click(function() {
+        if ($("#carousel-button").children("span").hasClass("fa-pause")) {
+            $("#mycarousel").carousel('pause');
+            $("#carousel-button").children("span").removeClass("fa-pause").addClass("fa-play");
+        } else {
+            $("#mycarousel").carousel('cycle');
+            $("#carousel-button").children("span").removeClass("fa-play").addClass("fa-pause");
+        }
+    });
+    $("#login").click(function() {
+        $("#loginModal").modal("show");
+    });
+
+    $("#reservetable").click(function() {
+        $("#reserveModal").modal("show");
+    });
+
+    $("#closereservemodal").click(function() {
+        $("#reserveModal").modal("hide");
+    })
+    $("#cancelreservemodal").click(function() {
+        $("#reserveModal").modal("hide");
+    })
+    $("#closeloginmodal").click(function() {
+       $("#loginModal").modal("hide");
+   })
+   $("#cancelloginmodal").click(function() {
+      $("#loginModal").modal("hide");
+  })
+
 });
