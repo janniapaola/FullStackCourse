1cd3471bcd9dc2995d7392632ac773009fb501f5
Jannia Paola Fajer <janniapaola@me.com>
2018-01-18 13:29:03 -0800

    Angular Commit Week 2

--- Angular/conFusion/src/app/app.module.ts
+++ Angular/conFusion/src/app/app.module.ts
@@ -10,22 +10,36 @@ import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
+import { DishService } from './services/dish.service';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    HttpModule,
-    MaterialModule,
-    FlexLayoutModule
-  ],
-  providers: [],
+      BrowserModule,
+      FormsModule,
+      HttpModule,
+      MaterialModule,
+      FlexLayoutModule,
+      AppRoutingModule
+    ],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
