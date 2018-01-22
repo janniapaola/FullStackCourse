bdbf833a46e485a59c251dd9f625c292e8015b81
Jannia Paola Fajer <janniapaola@me.com>
2018-01-21 19:42:54 -0800

    Angular Assignment 2

--- Angular/conFusion/src/app/app.module.ts
+++ Angular/conFusion/src/app/app.module.ts
@@ -1,10 +1,12 @@
+
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import 'hammerjs';
 
@@ -13,12 +15,13 @@ import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
-import { AppRoutingModule } from './app-routing/app-routing.module';
+import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
 
 @NgModule({
   declarations: [
@@ -27,19 +30,22 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     DishdetailComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent,
     AboutComponent,
+    HomeComponent,
     ContactComponent
   ],
   imports: [
-      BrowserModule,
-      FormsModule,
-      HttpModule,
-      MaterialModule,
-      FlexLayoutModule,
-      AppRoutingModule
-    ],
-  providers: [DishService],
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    AppRoutingModule
+  ],
+  providers: [DishService,
+    PromotionService,
+    LeaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
